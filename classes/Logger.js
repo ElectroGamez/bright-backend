8c01aa580d2f6752bcafa4146a2f816219b03eac
Wouter de Bruijn <git@electrogamez.nl>
2020-02-13 11:42:06 +0100

    Added loglevels

--- classes/Logger.js
+++ classes/Logger.js
@@ -1,14 +1,18 @@
 class Logger {
+    constructor(options) {
+        this.level = options.level || 2; // Info and lower
+    }
+
     debug(msg, obj) {
-        this.sendConsole("\x1b[1m[DEBUG]\x1b[0m ", msg, obj);
+        if (this.level >= 3) this.sendConsole("\x1b[1m[DEBUG]\x1b[0m ", msg, obj);
     }
 
     info(msg, obj) {
-        this.sendConsole("\x1b[1m[INFO]\x1b[0m ", msg, obj);
+        if (this.level >= 2) this.sendConsole("\x1b[1m[INFO]\x1b[0m ", msg, obj);
     }
 
     error(msg, obj) {
-        this.sendConsole("\x1b[1m\x1b[31m[ERROR]\x1b[0m ", msg, obj);
+        if (this.level >= 1) this.sendConsole("\x1b[1m\x1b[31m[ERROR]\x1b[0m ", msg, obj);
     }
     
     sendConsole(prefix, msg, obj, suffix) {
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Load/Start Logger
 const Logger = require('./classes/Logger.js');
-global.log = new Logger();
+global.log = new Logger({level: 3}); // Show debug messages
 
 //Load/Start Backend
 const Bright = require('./classes/Bright.js');
